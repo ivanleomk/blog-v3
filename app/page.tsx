f409ef4350f2ff9a1d8c245873ad4920b23f223d
Ivan Leo <ivanleomk@gmail.com>
2024-09-05 17:54:46 +0800

    Added short braintrust article

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,10 @@ export default function Page() {
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">Ivan Leo</h1>
       <p className="mb-4">
-        {`I'm a Software Engineer based out of Singapore working mainly with Typescript. In my spare time, I write about my experience building prototypes with LLMs, teaching myself ML and other things I've learnt along the way. Outside of machine learning, I love running, weightlifting and the great outdoors. :) `}
+        {`I'm currently a Research Engineer at 567 labs where I help prototype and experiment with application tooling involving LLMs. I primarily work with Python, though I have experience in JavaScript and Java. I maintain the instructor package for Python and spend my free time delving into AI research and experimenting with various tooling infrastructures.`}
+      </p>
+      <p>
+        {`Outside of tech, I enjoy running, weightlifting, and outdoor adventures. I often write about my experiences in ML and software engineering.`}
       </p>
       <div className="my-8">
         <BlogPosts />
